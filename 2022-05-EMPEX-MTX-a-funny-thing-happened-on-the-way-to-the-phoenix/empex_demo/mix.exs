5ff331e242f18d012859bb9583b4724b525cccef
Mat Trudel <mat@geeky.net>
2022-05-03 15:13:50 -0400

    Use bandit 0.5.0 in empex demo

--- 2022-05-EMPEX-MTX-a-funny-thing-happened-on-the-way-to-the-phoenix/empex_demo/mix.exs
+++ 2022-05-EMPEX-MTX-a-funny-thing-happened-on-the-way-to-the-phoenix/empex_demo/mix.exs
@@ -45,7 +45,8 @@ defmodule EmpexDemo.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:bandit, ">= 0.5.0"}
     ]
   end
 
